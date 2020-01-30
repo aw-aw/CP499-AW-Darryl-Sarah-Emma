4ca47272e87a93557aa760b663b6ebd3c1ed9105
emmablair <e_blair@ColoradoCollege.edu>
2020-01-30 15:01:32 -0700

    added CAS to test site

--- TestFlaskSite/TestFlaskSite.py
+++ TestFlaskSite/TestFlaskSite.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, json, request
+from flask_cas import CAS
+from flask_cas import login
+from flask_cas import logout
+from flask_cas import login_required
+import logging
 
 app = Flask(__name__)
+CAS(app)
+app.config['CAS_SERVER'] = 'https://cas.coloradocollege.edu/'
+app.config['CAS_AFTER_LOGIN'] = '/'
+
+
 
 @app.route('/')
-def main():
-    return render_template('index.html')
+@login_required
+def route_root():
+    return flask.render_template('index.html')
 
 @app.route('/preferences')
 def showPreferences():
@@ -15,4 +26,10 @@ def showProfile():
     return render_template('profile.html')
 
 if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    #sess.init_app(app)
+
+    app.debug = True
     app.run()
