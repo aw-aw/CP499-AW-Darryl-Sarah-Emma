882f2325aa70f57d5e5c09e28d9dab8beb7b4983
emmablair <e_blair@ColoradoCollege.edu>
2020-02-02 14:48:56 -0700

    updated UI files for QRCcal on server

---
+++ NewUiFiles/Main.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import SettingsIcon from '@material-ui/icons/Settings';
+import { Avatar } from "@material-ui/core";
+import { Button, Box } from "@material-ui/core";
+import {
+  Route,
+  NavLink,
+  BrowserRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Preferences from "./Preferences";
+import Profile from "./Profile";
+
+class Main extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Button color="secondary"><Avatar><SettingsIcon /></Avatar></Button>
+          <h1 id="sitetitle">QRCcal</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/preferences">Preferences</NavLink></li>
+            <li><NavLink to="/profile">Profile</NavLink></li>
+          </ul>
+          <div className="content">
+             <Route exact path="/" component={Home}/>
+	           <Route path="/preferences" component={Preferences}/>
+	           <Route path="/profile" component={Profile}/>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default Main;
