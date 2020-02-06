3c42d949a5873e4ab1d636ac4ff58bb91fbc8e5c
Sarah Dunbar <sarahdunbar@Sarahs-MacBook-Air-3.local>
2020-02-06 12:36:10 -0700

    actual new Preferences.js file

--- QRCcal/react_frontend/src/TutorFiles/PreferencesTutor.js
+++ QRCcal/react_frontend/src/TutorFiles/PreferencesTutor.js
@@ -40,15 +40,32 @@ const theme2 = createMuiTheme({
 })
 
 class ShiftPopUp extends Component{
-  render(){
-  return(
+  state = {
+    selected: null,
+    hasError: false
+  };
+
+
+  handleChange(value) {
+    this.setState({ selected: value });
+ }
+ handleClick() {
+    this.setState({ hasError: false });
+    if (!this.state.selected) {
+      this.setState({ hasError: true });
+    }
+  } 
+ render(){
+   const { classes } = this.props;
+   const { selected, hasError } = this.state;
+   return(
     <Grid item xs>
-        <Popup trigger={<Button size="large" color="primary" borderColor="secondary.main" variant="outlined" fullWidth="true">2-4</Button>} modal={true}>
+        <Popup trigger={<Button size="large" color="primary" borderColor="secondary.main" variant="outlined" fullWidth="true">{this.props.button}</Button>} modal={true}>
           {close => (
             <div>
               <h4>Current Preferences Entered By Tutors:</h4>
               <p>Put list of preferred shifts here: (users: discipline)</p>
-              <h4>Add Preferred Shift (Monday 2-4)</h4>
+              <h4>Add Preferred Shift {this.props.shift}</h4>
               <Grid item xs>
                 <FormControl style={{minWidth: 200}} error={hasError}>
                   <InputLabel>Discipline</InputLabel>
@@ -68,69 +85,51 @@ class ShiftPopUp extends Component{
 }
 
 class PreferencesTutor extends Component {
-  state = {
-    selected: null,
-    hasError: false
-  };
-
-
-  handleChange(value) {
-    this.setState({ selected: value });
-  }
-
-  handleClick() {
-    this.setState({ hasError: false });
-    if (!this.state.selected) {
-      this.setState({ hasError: true });
-    }
-  }
   render() {
-    const { classes } = this.props;
-    const { selected, hasError } = this.state;
     return (
       <div>
       <ThemeProvider theme={theme}>
-      <h1 align="center">Block 6 Preferred Shifts (Admin)</h1>
+      <h1 align="center">Block 6 Preferred Shifts (Tutor)</h1>
         <Grid container spacing={2}>
           <Grid item xs>
             <p>Monday: {moment().isoWeekday(1).format('MM/DD')}</p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Monday (2-4)" button="2-4"/>
             <p></p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Monday (4-6)" button="4-6"/>
             <p></p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Monday (6-8)" button="6-8"/>
             <p></p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Monday (8-10)" button="8-10"/>
           </Grid>
           <Grid item xs>
             <p>Tuesday: {moment().isoWeekday(2).format('MM/DD')}</p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Tuesday (2-4)" button="2-4"/>
             <p></p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Tuesday (4-6)" button="4-6"/>
             <p></p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Tuesday (6-8)" button="6-8"/>
             <p></p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Tuesday (8-10)" button="8-10"/>
           </Grid>
           <Grid item xs>
             <p>Wednesday: {moment().isoWeekday(3).format('MM/DD')}</p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Wednesday (2-4)" button="2-4"/>
             <p></p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Wednesday (4-6)" button="4-6"/>
             <p></p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Wednesday (6-8)" button="6-8"/>
             <p></p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Wednesday(8-10)" button="8-10"/>
           </Grid>
           <Grid item xs>
             <p>Thursday: {moment().isoWeekday(4).format('MM/DD')}</p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Thursday (2-4)" button="2-4"/>
             <p></p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Thursday (4-6)" button="4-6"/>
             <p></p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Thursday (6-8)" button="6-8"/>
             <p></p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Thursday (8-10)" button="8-10"/>
           </Grid>
           <Grid item xs>
             <p>Friday: {moment().isoWeekday(5).format('MM/DD')}</p>
@@ -140,13 +139,13 @@ class PreferencesTutor extends Component {
           </Grid>
           <Grid item xs>
             <p>Sunday: {moment().isoWeekday(7).format('MM/DD')}</p>
-            <ShowPopUp/>
+            <ShiftPopUp shift="Sunday (2-4)" button="2-4"/>
             <p></p>
-            <ShowPopUp/>
+            <ShiftPopUp shift="Sunday (4-6)" button="4-6"/>
             <p></p>
-            <ShowPopUp/>
+            <ShiftPopUp shift="Sunday (6-8)" button="6-8"/>
             <p></p>
-            <ShiftPopUp/>
+            <ShiftPopUp shift="Sunday (8-10)" button="8-10"/>
             </Grid>
           </Grid>
       </ThemeProvider>
